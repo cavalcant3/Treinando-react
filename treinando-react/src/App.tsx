e33efef9ede048df2402ffe3618e92425967e3ea
cavalcant3 <jmatheusoc269@gmail.com>
2021-10-17 14:38:12 -0300

    brincando com useState

--- treinando-react/src/App.tsx
+++ treinando-react/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header'
 import Menu from './components/Menu';
 
 
 function App() {
+
+  const [counter, setCounter] = useState(1);
+  
+  //incrementar
+  function handleButtonClick(){
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+  //diminuir
+  function RemoveButtonClick(){
+    setCounter(counter -1);
+    console.log(counter);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +26,11 @@ function App() {
         <p>
           E ai pessoal
         </p>
+          <p> { counter } </p>
+          {/* incrementar */}
+          <button onClick={handleButtonClick}>Adicionar mais itens</button>
 
+          <button onClick={RemoveButtonClick}>Remover mais itens</button>
         <Menu>
           <ul>
             <li>Cardapio</li>
